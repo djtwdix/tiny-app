c6adc4d12a474d7c2e06b05b76a1b7b777059b2c
Daniel Tweedie <djtwd.ix@icloud.com>
2021-02-15 19:37:38 +0000

    Added urls_new.ejs with form for shortening URLs and enabled the POST request on server side, implemented generateRandomString that generates a random 6 digit alphanumeric string

--- express_server.js
+++ express_server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = 8080;
+
+const generateRandomString = () => {
+  let randomString = ""
+  let alphaNum = '123456789abcdefghijklmnopqrstuvwxyz'
+  for (let i = 0; i <= 6; i++) {
+  	randomString += alphaNum[Math.round(Math.random() * (alphaNum.length - 1))];
+  }
+  return randomString;
+}
+
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs');
 
@@ -25,6 +38,19 @@ app.get("/urls", (req,res) => {
 app.get("/urls/:shortURL", (req,res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase.shortURL}
   res.render("urls_show", templateVars);
+})
+
+app.get("/urls_new", (req,res) => {
+  res.render("urls_new");
+})
+
+/* app.get("/urls/:id", (req,res) => {
+  res.render(urls_new);
+}) */
+
+app.post("/urls", (req,res) => {
+  console.log(req.body);
+  res.send("Ok");
 })
 
 app.get("/hello", (req,res) => {
